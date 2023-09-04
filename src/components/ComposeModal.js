fed07bc47015b7e0ac026980820180e288a284db
Janelle <janellephalon@gmail.com>
2023-09-04 13:59:29 -0400

    working compose button

---
+++ src/components/ComposeModal.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
+import axios from 'axios';  // Import axios
+
+const ComposeModal = () => {
+    const [show, setShow] = useState(false);
+    const [recipient, setRecipient] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleDraftSave = async () => {
+      try {
+          const response = await axios.post('http://localhost:5000/draft-message', {
+              to: [recipient],
+              subject: subject,
+              textBody: message
+              // You can expand on this object based on the fields DataMotion expects
+          });
+
+          console.log('Draft response:', response.data);
+
+          alert('Draft saved successfully!');
+      } catch (error) {
+          console.error('Error saving draft:', error);
+          alert('Error saving draft. Please try again.');
+      }
+
+      handleClose();
+  };
+
+    const handleSend = () => {
+        // Logging the data for now
+        console.log('Sending Email');
+        console.log('Recipient:', recipient);
+        console.log('Subject:', subject);
+        console.log('Message:', message);
+
+        // Later, you can integrate the backend API call here for sending the email
+
+        handleClose();
+    };
+
+    return (
+        <>
+          <div className='col-md-12'>
+            <Button variant="primary" onClick={handleShow} className='btn' style={{ marginBottom: '20px', width: '100%' }}>
+                Compose
+            </Button>
+          </div>
+          <div className='modal-style'>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title className='modal-title'>Compose Email</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>To:</Form.Label>
+                            <Form.Control type="email" value={recipient} onChange={(e) => setRecipient(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Subject:</Form.Label>
+                            <Form.Control type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Message:</Form.Label>
+                            <Form.Control as="textarea" rows={3} value={message} onChange={(e) => setMessage(e.target.value)} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleDraftSave}>
+                        Save as Draft
+                    </Button>
+                    <Button variant="primary" onClick={handleSend}>
+                        Send
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+          </div>
+        </>
+    );
+};
+
+export default ComposeModal;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, ListGroup } from 'react-bootstrap';
 
+// Components
+import ComposeModal from './ComposeModal';
+
 const Sidebar = () => {
-    return (
-      <div className="col-md-3 pt-3">
-            <Button variant="primary" size="lg" style={{ marginBottom: '20px', width: '100%' }} className="btn">
-                Compose
-            </Button>
-            <div className=''>
-            <ListGroup className="sidebar-styles">
-                <ListGroup.Item action href="#inbox" className="">
-                    Inbox
-                </ListGroup.Item>
-                <ListGroup.Item action href="#sent" className="">
-                    Sent
-                </ListGroup.Item>
-                <ListGroup.Item action href="#drafts" className="">
-                    Drafts
-                </ListGroup.Item>
-                <ListGroup.Item action href="#spam" className="">
-                    Spam
-                </ListGroup.Item>
-                <ListGroup.Item action href="#trash" className="">
-                    Trash
-                </ListGroup.Item>
-            </ListGroup>
-            </div>
-        </div>
-    );
+
+  const [showComposeModal, setShowComposeModal] = useState(false);
+
+  const handleComposeClick = () => {
+    setShowComposeModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowComposeModal(false);
+  };
+
+  return (
+    <div className="col-md-3 pt-3">
+      <ComposeModal show={showComposeModal} onClick={handleComposeClick} onClose={handleCloseModal} />
+
+      <div className=''>
+        <ListGroup className="sidebar-styles">
+          <ListGroup.Item action href="#inbox" className="">
+            Inbox
+          </ListGroup.Item>
+          <ListGroup.Item action href="#sent" className="">
+            Sent
+          </ListGroup.Item>
+          <ListGroup.Item action href="#drafts" className="">
+            Drafts
+          </ListGroup.Item>
+          <ListGroup.Item action href="#spam" className="">
+            Spam
+          </ListGroup.Item>
+          <ListGroup.Item action href="#trash" className="">
+            Trash
+          </ListGroup.Item>
+        </ListGroup>
+
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
