21d84fe7fdc029d0da16183c48d4a1813cf5d2f1
Janelle <janellephalon@gmail.com>
2023-09-08 10:42:19 -0400

    updated nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,64 @@
 import React from 'react';
 import { Navbar, Nav, NavItem, NavLink } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDev, faLinkedin, faTwitterSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
-import dmLogo from '../assets/DataMotion-Logo-White-1 copy (1).png'
+import dmLogo from '../assets/DataMotion-Logo-White-1 copy (1).png';
 
 const NavigationBar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-            <Navbar.Brand href="https://datamotion.com/" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '20px' }}>
-                <img
-                    alt="DataMotion logo"
-                    src={dmLogo}
-                    height="30"
-                    className="d-inline-block align-top"
-                />
-            </Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="flex-column">
+            <div className="d-flex w-100 justify-content-between">
+                <Navbar.Brand href="https://datamotion.com/" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '20px' }}>
+                    <img
+                        alt="DataMotion logo"
+                        src={dmLogo}
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <NavItem>
-                        <NavLink href="https://www.linkedin.com/in/janelle-phalon-7b743a157/" target="_blank" rel="noopener noreferrer">
-                            LinkedIn
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="https://twitter.com/devreljanelle" target="_blank" rel="noopener noreferrer">
-                            Twitter
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="https://dev.to/janellephalon" target="_blank" rel="noopener noreferrer">
-                            Dev Blog
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="https://github.com/janellephalon" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <NavItem>
+                            <NavLink href="https://www.linkedin.com/in/janelle-phalon-7b743a157/" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://twitter.com/devreljanelle" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faTwitterSquare} />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://dev.to/janellephalon" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faDev} />
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/janellephalon" target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faGithubSquare} />
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+
+            <div className="w-100 d-flex justify-content-start pl-4" style={{ marginLeft: '35px' }}>
+                <span className="badge badge-light badge-pill mr-2">
+                    Secure Message Center API
+                </span>
+                <span className="badge badge-pill">
+                    React
+                </span>
+                <span className="badge badge-pill">
+                    Bootstrap
+                </span>
+                <span className="badge badge-pill">
+                    CSS
+                </span>
+            </div>
         </Navbar>
     );
 };
